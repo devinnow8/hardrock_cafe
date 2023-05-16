dd8985ff158ba5d1af6cfef58e44cff95dd1c9bf
Anoop <apatel@innow8apps.com>
2023-05-16 18:00:52 +0530

    add cart

--- src/Component/Cartitem.js
+++ src/Component/Cartitem.js
@@ -1,16 +1,23 @@
 import React from "react";
+import Burger from "../assets/image/Burger.jpeg";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 function Cartitem() {
   return (
-    <div>
-      <div>Cartitem</div>
-      <div classname="product-info">
-       
-      </div>
+    <div className="middle">
       <div className="title">
-      <h2> Burger</h2>
-      <h3> Best in town</h3>
-      
+        <div className="product-info">
+          <img src={Burger} alt="burger" width="100" height="100" />
+        </div>
       </div>
+      <div className="Textcenter">
+        <h2> Burger</h2>
+        <div className="PlusMinus">
+          <AiOutlinePlusCircle />
+          <input className="count" type="text" placeholder="2" />
+          <AiOutlineMinusCircle />
+        </div>
+      </div>
+      <span>delete</span>
     </div>
   );
 }
